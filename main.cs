a0379fd6254060c55f43e7abb79e343ad473441f
Cannonbarrel <Firenoob1001@gmail.com>
2024-06-10 03:02:37 +0000

    My program for exercise 3.1

--- main.cs
+++ main.cs
@@ -2,6 +2,21 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
- 
+    Console.Write("Enter your message: ");
+        string userMessage = Console.ReadLine();
+
+        if (CheckMessageLength(userMessage))
+        {
+            Console.WriteLine("Posted");
+        }
+        else
+        {
+            Console.WriteLine("Rejected");
+        }
+    }
+
+    static bool CheckMessageLength(string message)
+    {
+        return message.Length <= 140;
   }
 }
